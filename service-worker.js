51d545bde36143c5450491deae3f51bc48451816
Jacson Costa <104026932+DetonaJacs@users.noreply.github.com>
2024-10-29 20:18:02 -0300

    Update and rename sw.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './styles/default.css',
+        './img/logo.png'
+        // Adicione outros recursos aqui, como JavaScript, fontes, etc.
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
